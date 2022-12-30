9eb6d35ec0bc2fcd06370e262cfc01220cdd988e
AhmadEmad7 <121223405+AhmadEmad7@users.noreply.github.com>
2022-12-30 21:00:28 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscreteProject2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, sum ;
+
+            Console.Write("Enter First number: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second number: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+            for (int i = n1; i <= n2; i++)
+            {
+                int j = 1;
+                sum = 0;
+                while (j < i)
+                {
+                    if (i % j == 0) sum += j;
+                    j++;
+                }
+                if (sum == i) Console.Write("{0} , ", i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
